a1fb2828859009ed3dd31952073783f4a17fe61c
jamesmannelly <77638484+jamesmannelly@users.noreply.github.com>
2021-06-16 12:24:55 -0700

    Update DirectoryListingTest.java

--- src/test/java/io/harness/jhttp/processor/DirectoryListingTest.java
+++ src/test/java/io/harness/jhttp/processor/DirectoryListingTest.java
@@ -62,7 +62,7 @@ public class DirectoryListingTest {
         when(request.resolvePath()).thenReturn(tempDir.resolve("some-dir"));
         when(request.getUri()).thenReturn("/some-dir");
         assertTrue(new DirectoryListing().process(request, response));
-//        assertEquals(readResource("/directory-listing/some-dir.html"), stringWriter.toString());
+        assertEquals(readResource("/directory-listing/some-dir.html"), stringWriter.toString());
     }
 
     private String readResource(String filename) throws IOException {
